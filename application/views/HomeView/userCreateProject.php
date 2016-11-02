9f195b856c4fa0189553f53e8ca30545b471cb86
Artur <art.zgmt@gmail.com>
2016-11-02 01:22:52 +0100

    Data passing not as JSON

--- application/views/HomeView/userCreateProject.php
+++ application/views/HomeView/userCreateProject.php
@@ -1,10 +1,3 @@
-<div class="container">
-    <h2> Moje projekty</h2>
-    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-        Utwórz projekt
-    </button>
-</div>
-
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
      aria-hidden="true">
     <div class="modal-dialog modal-lg">
@@ -30,8 +23,6 @@
             <div class="modal-footer">
                 <?php echo form_submit('createProject','Utwórz',array('class'=>'btn btn-primary')) ?>
                 <?php echo form_submit('createProject','Anuluj',array('class'=>'btn btn-default', 'data-dismiss'=>'modal')) ?>
-
-
             </div>
             <?php echo form_close(); ?>
 
@@ -39,27 +30,63 @@
     </div>
 </div>
 
-<table class="table">
-    <thead class="thead-inverse">
+<div id="custom-toolbar">
+<!--        <button class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">-->
+<!--            Utwórz projekt-->
+<!--        </button>-->
+    <?php echo form_open('project') ?>
+    <?php echo form_submit('editProject','Edytuj',array('class'=>'btn btn-primary', 'id'=>'editProject','onclick'=>"getToEdit()")); ?>
+    <?php echo form_close(); ?>
+<!--        <button class="btn btn-primary" id="getToEdit">Edytuj</button>-->
+<!--       <button type="submit" class="btn btn-danger"  value="">Usuń</button>-->
+<!--    <button type="submit" class="btn btn-success">Wybierz</button>-->
+</div>
+<div id="custom-bottom">
+
+</div>
+<div class="container">
+<h2> Moje projekty</h2>
+<table id="projectTable" data-toggle="table" data-toolbar="#custom-toolbar"
+       data-search="true" data-pagination="true"
+       data-height="600" data-show-refresh="true"
+       data-click-to-select="true"
+       data-single-select="true" >
+    <thead>
     <tr>
-        <th>#</th>
-        <th>Nazwa Projektu</th>
+        <th data-checkbox="true">#</th>
+        <th  data-field="name">Nazwa Projektu</th>
         <th>Opis Projektu</th>
+        <th>test</th>
     </tr>
     </thead>
     <tbody>
     <?php
-        $i = 0 ;
-        foreach  ($userProject as $value)
-        {
-            $i++;
-            echo '<tr>
-            <th scope="row">'.$i.'</th>
+    $i = 0 ;
+    foreach  ($userProjectData as $value)
+    {
+        $i++;
+        echo '
+        <tr>
+            <td>'.$i.'</td>
             <td>'.$value['name'].'</td>
             <td>'.$value['description'].'</td>
+            <td>'.$value['idProject'].'</td>
         </tr>';
-        }?>
-
+    }?>
     </tbody>
 </table>
+</div>
+
+<script>
+    function getToEdit()
+    {
+        var value =  JSON.stringify( $('#projectTable').bootstrapTable('getSelections'));
+        document.getElementById("editProject").value = value;
+    }
+</script>
+
+
+
+
+
 
